8be672fe996213095c6e644a0955517d4397b60c
josevillista <jbarrientosvela@gmail.com>
2022-10-18 22:43:03 +0200

    I have modified js, added images and modified css

--- FormFornite/js/myscript.js
+++ FormFornite/js/myscript.js
@@ -1,50 +1,73 @@
+var valido = false;
 
-
-
-
-
-function validaDatos() {
-
-        var validacion = true;
+function validaEdad() {
         var anio = document.getElementById("anio").value;
+        var campoAnio = document.getElementById("anio");
         if (anio > 2006) {
                 alert("No puedes registrarte, tienes menos de 16 años");
-                validacion = false;
+                campoAnio.style.border = "red solid 3px";
+                document.getElementById("checkAnio").style.display = 'none';
+                valido = false;
+        } else {
+                campoAnio.style.border = "none";
+                document.getElementById("checkAnio").style.display = 'block';
+                valido = true;
         }
-        var dni = document.getElementByIdy("dni").value;
-        var campoDNI = document.getElementByIdy("dni");
+}
+function validaDNI() {
+        var dni = document.getElementById("dni").value;
+        var campoDNI = document.getElementById("dni");
         var reg = /^[0-9]{8}[- ]?[A-Z]{1}$/;
-        if (!reg.test(dni)) {
-                alert("formato DNI incorrecto");
+        if (!dni.match(reg)) {
                 campoDNI.style.border = "red solid 3px";
-                validacion = false;
+                document.getElementById("checkDNI").style.display = 'none';
+                valido = false;
+        } else {
+                campoDNI.style.border = "none";
+                document.getElementById("checkDNI").style.display = 'block';
+                valido = true;
         }
-        var email = document.getElementByIdy("email").value;
-        var campoEmail = document.getElementByIdy("email");
+}
+function validaEmail() {
+
+        var email = document.getElementById("email").value;
+        var campoEmail = document.getElementById("email");
         var reg = /^[a-zA-Z0-9]{1,64}\S+@\D\S+\.\D\S+$/;
         if (!reg.test(email)) {
-                alert("formato Email incorrecto");
                 campoEmail.style.border = "red solid 3px";
-                validacion = false;
+                document.getElementById("checkEmail").style.display = 'none';
+                valido = false;
+        } else {
+                campoEmail.style.border = "none";
+                document.getElementById("checkEmail").style.display = 'block';
+                valido = true;
         }
+}
+function validaTlf() {
 
         var tlf = document.getElementById("tlf").value;
         var campoTlf = document.getElementById("tlf");
         var reg = /^[0-9]{3}[\s-]?[0-9]{3}[\s-]?[0-9]{3}$/;
         if (!reg.test(tlf)) {
-                alert("formato Teléfono incorrecto");
                 campoTlf.style.border = "red solid 3px";
-                validacion = false;
+                document.getElementById("checkTlf").style.display = 'none';
+                valido = false;
+        } else {
+                campoTlf.style.border = "none";
+                document.getElementById("checkTlf").style.display = 'block';
+                valido = true;
         }
 }
 
 function muestraCredenciales() {
-        if (validacion) {
+
+        if (valido) {
                 document.getElementById("datosPers").style.display = 'none';
                 document.getElementById("credenciales").style.display = 'block';
         } else {
-                alert("Datos incorrectos");
+                alert("Campos incorrecto");
         }
+
 }
 function atrasCredenciales() {
         document.getElementById("datosPers").style.display = 'block';
@@ -55,41 +78,25 @@ function atrasCuenta() {
         document.getElementById("credenciales").style.display = 'block';
 }
 function muestraCuenta() {
-        if (validacion) {
-                document.getElementById("cuenta").style.display = 'block';
-                document.getElementById("credenciales").style.display = 'none';
-        } else {
-                alert("Datos incorrectos");
-        }
+
+        document.getElementById("cuenta").style.display = 'block';
+        document.getElementById("credenciales").style.display = 'none';
+
 }
 
 function validaCuenta() {
-
 
         var iban = document.getElementById("iban").value;
         var campoIban = document.getElementById("iban");
         var reg = /^[A-Z]{2}[0-9]{2}[\s-]?[0-9]{20}$/;
         if (!reg.test(iban)) {
                 campoIban.style.border = "red solid 3px";
-                validacion = false;
         }
         var swift = document.getElementById("swift").value;
         var campoSwift = document.getElementById("swift");
         var reg = /^[A-Za-z]{8}[0-9]{3}?$/;
         if (!reg.test(swift)) {
                 campoSwift.style.border = "red solid 3px";
-                validacion = false;
-        }
-
-
-}
-function enviar() {
-        if (validacion) {
-                alert("Formulario enviado CORRECTAMENTE");
-                window.location = "https://www.epicgames.com/fortnite/es-ES/home";
-        } else {
-                alert("Formulario incorrecto");
-                window.location = "index.html";
         }
 }
 
